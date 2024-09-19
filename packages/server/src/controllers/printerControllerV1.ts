0ed33319ccc28acb91be84afcec609b27b54c733
Yongmin Kim <yongmin86k@gmail.com>
2024-09-18 22:47:37 -0700

    polling response

--- packages/server/src/controllers/printerControllerV1.ts
+++ packages/server/src/controllers/printerControllerV1.ts
@@ -1,9 +1,15 @@
+import { Request as ExpressRequest, Response as ExpressRespons } from 'express'
 import { API_VERSION } from 'star-cloud-printing-shared'
 import { TAddCustomRoute } from '../modules/jsonServer'
 
 export const addPrinterRoutesV1: TAddCustomRoute = (server, router) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const testRouter = router as unknown as {
+    render: (request: ExpressRequest, response: ExpressRespons) => void
+  }
   const printers = router.db.get('printers').value()
 
+  // ref: https://star-m.jp/products/s_print/sdk/StarCloudPRNT/manual/en/protocol-reference/http-method-reference/server-polling-post/json-request.html#post
   server.use(async (req, res, next) => {
     if (
       req.method === 'POST' &&
@@ -13,16 +19,34 @@ export const addPrinterRoutesV1: TAddCustomRoute = (server, router) => {
 
       body.lastConnected = query.t
 
-      const hasPrinter = printers.find(
+      const existingPrinter = printers.find(
         (printers) => printers.printerMAC === body.printerMAC
       )
 
-      if (hasPrinter) {
-        req.url = `/printers/${hasPrinter.id}`
+      if (existingPrinter) {
         req.method = 'PATCH'
+        req.url = `/printers/${existingPrinter.id}`
       }
     }
 
     next()
   })
+
+  testRouter.render = (req, res) => {
+    const { method, originalUrl } = req
+
+    if (
+      (method === 'PATCH' || method === 'POST') &&
+      originalUrl.includes(`${API_VERSION.V1}/printers`)
+    ) {
+      res.jsonp({
+        jobReady: false,
+      })
+    }
+  }
 }
+
+// other references
+// https://star-m.jp/products/s_print/sdk/StarCloudPRNT/manual/en/protocol-reference/http-method-reference/server-info-get/get-request.html#endpoint
+// https://star-m.jp/products/s_print/sdk/StarCloudPRNT/manual/en/protocol-reference/http-method-reference/server-polling-post/json-response.html
+// https://star-m.jp/products/s_print/sdk/StarCloudPRNT/manual/en/protocol-reference/common-spec-reference/printer-status-code/index.html#printer-status-code
